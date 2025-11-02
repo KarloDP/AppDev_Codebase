cbad89dc28e25401139dd8363e35bdb2d964911b
Kenneth Miguel Gumayagay <kennethmiguelgumayagay@gmail.com>
2025-11-02 21:33:43 +0800

    integerToRoman class and tester

---
+++ src/test/integerToRomanTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import Gumayagay_Kenneth.integerToRoman;
+
+public class integerToRomanTest {
+    public static void main(String[] args) {
+        IntegerToRoman converter = new IntegerToRoman();
+
+        int[] testNumbers = {3, 9, 58, 1994, 2024};
+
+        System.out.println("Integer to Roman Numeral Conversion:");
+        for (int num : testNumbers) {
+            System.out.println(num + " -> " + converter.intToRoman(num));
+        }
+    }
+}
