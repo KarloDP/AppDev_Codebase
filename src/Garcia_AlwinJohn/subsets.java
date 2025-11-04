c730ae9bc46eac93529a56bd1bea316c4202b6bd
Alwin John M. Garcia <garciaalwinj56@gmail.com>
2025-11-04 21:26:13 +0800

    added the missing codes

---
+++ src/Garcia_AlwinJohn/subsets.java
@@ -0,0 +1,22 @@
+package Garcia_AlwinJohn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+
+        for (int num : nums) {
+            List<List<Integer>> newSubsets = new ArrayList<>();
+            for (List<Integer> subset : result) {
+                List<Integer> newSubset = new ArrayList<>(subset);
+                newSubset.add(num);
+                newSubsets.add(newSubset);
+            }
+            result.addAll(newSubsets);
+        }
+        return result;
+    }
+}
