b66f1acf0b86c56e14d9ba269408bf46bd1f4990
Gideon Junio <2236216@slu.edu.ph>
2025-11-04 20:40:11 +0800

    added romantoInteger

---
+++ src/Junio_Gideon/romanToInteger.java
@@ -0,0 +1,34 @@
+package Junio_Gideon;
+
+public class romanToInteger {
+
+    public static int romanToInt(String s) {
+        int total = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currentValue = getValue(s.charAt(i));
+            if (currentValue < prevValue) {
+                total -= currentValue;
+            } else {
+                total += currentValue;
+            }
+            prevValue = currentValue;
+        }
+
+        return total;
+    }
+
+    private static int getValue(char rom) {
+        return switch (rom) {
+            case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X' -> 10;
+            case 'L' -> 50;
+            case 'C' -> 100;
+            case 'D' -> 500;
+            case 'M' -> 1000;
+            default -> 0;
+        };
+    }
+}
